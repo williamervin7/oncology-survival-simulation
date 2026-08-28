43d95c22d491cbecdfcddc80344bf575dce15cb1
williamervin7 <william7ervin@gmail.com>
2026-08-28 18:31:00 -0500

    feat: add initial test for data cleaning functions and create README for CI/CD workflows

---
+++ tests/software/test_cleaning.py
@@ -0,0 +1,13 @@
+import pytest
+import numpy as np
+import pandas as pd
+from src.data.cleaning import convert_missing_values, derive_stage_2018
+
+# this test is a placeholder for future tests related to data cleaning and transformation functions
+
+# after implementing the cleaning functions this test will check if the shape of the cleaned dataframe is as expected
+def test_shape():
+    pass
+
+
+
